17161e4cb4e3eba9c263f0d4374c87af032afd22
Phyrise <arthur.longuefosse@gmail.com>
2025-07-21 18:16:23 +0200

    correction : predict, get decoder_type from files

--- nnunetv2/inference/predict_from_raw_data.py
+++ nnunetv2/inference/predict_from_raw_data.py
@@ -101,7 +101,7 @@ class nnUNetPredictor(object):
         num_input_channels = determine_num_input_channels(plans_manager, configuration_manager, dataset_json)
         trainer_class = recursive_find_python_class(join(nnunetv2.__path__[0], "training", "nnUNetTrainer"),
                                                     trainer_name, 'nnunetv2.training.nnUNetTrainer')
-
+        decoder_type = configuration_manager.network_arch_init_kwargs["decoder_type"]
         network = trainer_class.build_network_architecture(
             configuration_manager.network_arch_class_name,
             configuration_manager.network_arch_init_kwargs,
